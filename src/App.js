f320e4eba45a0a025ffa9524fdfd72ed9c837cb8
SEUNGMIN KIM <smk950226@naver.com>
2018-10-05 19:22:37 +0900

    router 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const App = () => null;
+const App = () => (
+  <div>
+    <Header />
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>My Contacts</h1>
+  </header>
+)
 
 export default App;
