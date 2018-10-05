081d1b87a2bfe9085ea1fcc8f2ffb68dc1bd9799
SEUNGMIN KIM <smk950226@naver.com>
2018-10-05 19:29:48 +0900

    Route 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
@@ -9,7 +10,21 @@ const App = () => (
 const Header = () => (
   <header>
     <h1>My Contacts</h1>
+    <Route exact path="/" component={Welcome} />
+    <Route path='/contacts' component={Contacts} />
   </header>
+)
+
+const Welcome = () => (
+  <h1>Welcome to the best contacts app!</h1>
+);
+
+const Contacts = () => (
+  <ul>
+    <li>Lynn</li>
+    <li>I</li>
+    <li>My</li>
+  </ul>
 )
 
 export default App;
